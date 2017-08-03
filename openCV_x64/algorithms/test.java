12f02f08c2cb36a73ede0c7b19967ca22ef1436b
zylo117 <zylo117@hotmail.com>
2017-08-03 10:48:33 +0800

    openCV works

---
+++ openCV_x64/algorithms/test.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+import org.opencv.core.CvType;  
+import org.opencv.core.Mat;  
+public class test {  
+    /** 
+     * @param args 
+     */  
+    public static void main(String[] args) {  
+        // TODO Auto-generated method stub  
+        System.loadLibrary("opencv_java320");  
+        Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);  
+        System.out.println("m = " + m.dump());  
+    }  
+  
+}  
