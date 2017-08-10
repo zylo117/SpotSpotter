be6acb85f00887986142dd09ceb78de003995580
zylo117 <zylo117@hotmail.com>
2017-08-10 11:05:33 +0800

    ROI works

--- src/pers/zylo117/spotspotter/patternrecognition/regiondetector/DetectCorners.java
+++ src/pers/zylo117/spotspotter/patternrecognition/regiondetector/DetectCorners.java
@@ -6,13 +6,15 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 public class DetectCorners {
 
-	public static void corners(String inputimg, int roi_startX, int roi_startY, int width, int height, double density,
-			String outputimg) {
+	public static Point[] pCorners;
+	public static Mat srcROI;
+
+	public static Mat corners(Mat src, int roi_startX, int roi_startY, int width, int height, double density,
+			boolean ifOutputRawPoint) {
 		try {
 			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
@@ -22,25 +24,27 @@ public class DetectCorners {
 			MatOfPoint corners = new MatOfPoint();
 
 			// …Ë÷√ROI
-			Mat src = Imgcodecs.imread(inputimg);
 			Rect rect = new Rect(roi_startX, roi_startY, width, height);
-			Mat srcROI = new Mat(src, rect);
+			srcROI = new Mat(src, rect);
+			
 			if (srcROI.empty()) {
 				throw new Exception("no file");
 			}
-			Mat dst = srcROI.clone();
 			Mat gray = new Mat();
 
 			Imgproc.cvtColor(srcROI, gray, Imgproc.COLOR_RGB2GRAY);
 			Imgproc.goodFeaturesToTrack(gray, corners, maxCorners, qualityLevel, minDistance, new Mat(), blockSize,
 					useHarrisDetector, k);
-			Point[] pCorners = corners.toArray();
-			for (int i = 0; i < pCorners.length; i++) {
-				Imgproc.circle(srcROI, pCorners[i], 4, new Scalar(255, 255, 0), 2);
+			pCorners = corners.toArray();
+
+			if (ifOutputRawPoint) {
+				for (int i = 0; i < pCorners.length; i++) {
+					Imgproc.circle(srcROI, pCorners[i], 4, new Scalar(255, 255, 0), 2);
+				}
 			}
-			Imgcodecs.imwrite(outputimg, src);
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
 		}
+		return src;
 	}
 }
--- src/pers/zylo117/spotspotter/patternrecognition/regiondetector/test.java
+++ src/pers/zylo117/spotspotter/patternrecognition/regiondetector/test.java
@@ -1,26 +1,60 @@
 package pers.zylo117.spotspotter.patternrecognition.regiondetector;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import pers.zylo117.spotspotter.toolbox.GetMaxMin;
+
 public class test {
 
 	// Pythagoras-G
 	// 推荐参数
 	//
 
-//	public static void Pythagoras_G(String input, String output) {
+	// public static void Pythagoras_G(String input, String output) {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		String a = "D:\\workspace\\SpotSpotter\\src\\pers\\zylo117\\spotspotter\\image\\1.jpg";
-		String b = "D:\\workspace\\SpotSpotter\\src\\pers\\zylo117\\spotspotter\\image\\output1.jpg";
+		String input = "D:/workspace/SpotSpotter/src/pers/zylo117/spotspotter/image/2.jpg";
+		String output = "D:/workspace/SpotSpotter/src/pers/zylo117/spotspotter/image/output2.jpg";
+
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat src = Imgcodecs.imread(input);
+
 		// Upper-Left corner
-		DetectCorners.corners(a, 206, 178, 20, 20, 15.0, b);	
+		src = DetectCorners.corners(src, 206, 178, 20, 20, 15.0, false);
+
+		// 求最优解
+		double ulpoint = 0;
+		List<Double> list = new ArrayList<Double>();
+		for (int i = 0; i < DetectCorners.pCorners.length; i++) {
+			list.add(DetectCorners.pCorners[i].x);
+		}
+		ulpoint = GetMaxMin.getMax(list);
+		// 求最大值的点的序号
+		int pointNO;
+		for(pointNO = 0; pointNO < DetectCorners.pCorners.length; pointNO++ ) {
+			if (ulpoint == DetectCorners.pCorners[pointNO].x) {
+				break;
+			}
+		}
+		System.out.println(ulpoint);
+		System.out.println(pointNO);
+		Imgproc.circle(DetectCorners.srcROI, DetectCorners.pCorners[pointNO], 4, new Scalar(255, 255, 0), 2);
+
 		// Upper-Right corner
-		DetectCorners.corners(a, 306, 178, 20, 20, 1.0, b);
+		src = DetectCorners.corners(src, 306, 178, 20, 20, 1.0, false);
 		// Lower-Left corner
-		DetectCorners.corners(a, 206, 326, 20, 20, 15.0, b);
+		src = DetectCorners.corners(src, 206, 326, 20, 20, 15.0, false);
 		// Lower-Right corner
-		DetectCorners.corners(a, 306, 326, 20, 20, 1.0, b);
-		
-		
+		src = DetectCorners.corners(src, 306, 326, 20, 20, 1.0, false);
+
+		Imgcodecs.imwrite(output, src);
 
 	}
 }
