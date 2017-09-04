d03ee547f021a3c10d288a80fcd2adebc76352aa
zylo117 <zylo117@hotmail.com>
2017-09-04 15:04:30 +0800

    fix

--- src/pers/zylo117/spotspotter/dataio/output/email/Mail_SMTP.java
+++ src/pers/zylo117/spotspotter/dataio/output/email/Mail_SMTP.java
@@ -64,8 +64,8 @@ public class Mail_SMTP {
 	// 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）, 
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-    public static String myEmailAccount = "Jiahao.Zhang@o-film.com";
-    public static String myEmailPassword = "z1z1z1``````";
+    public static String myEmailAccount = "123@abc.com";
+    public static String myEmailPassword = "123456";
 
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     // 网易163邮箱的 SMTP 服务器地址为: smtp.163.com
