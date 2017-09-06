fa16905e059be22475681f7fbe437a62983f4f45
zylo117 <zylo117@hotmail.com>
2017-09-06 17:11:52 +0800

    Alpha 0.4.0.1
    Set Default View

--- src/pers/zylo117/spotspotter/gui/viewer/CentralControl.java
+++ src/pers/zylo117/spotspotter/gui/viewer/CentralControl.java
@@ -28,11 +28,13 @@ import pers.zylo117.spotspotter.toolbox.GetPostfix;
 import pers.zylo117.spotspotter.toolbox.Mat2BufferedImage;
 
 public class CentralControl extends JFrame {
-	
-	public static JTextField processName_manual, machineNO_manual, productName_manual, binarizationThreshold, spotSpotterThreshold;
+
+	public static JTextField processName_manual, machineNO_manual, productName_manual, binarizationThreshold,
+			spotSpotterThreshold;
 	public static String productN = "XX";
 	public static int mcNO = 0, binThresh = 20, ssThresh = 15;
 	public static int algoIndex = 2;
+
 	/**
 	 * Display Mat image
 	 *
@@ -54,7 +56,7 @@ public class CentralControl extends JFrame {
 
 		JFrame jFrame = new JFrame(windowName);
 		JLabel imageView = new JLabel();
-		
+
 		JFileChooser chooser = new JFileChooser();
 		chooser.setCurrentDirectory(new File("."));
 		JMenuBar menubar = new JMenuBar();
@@ -98,35 +100,35 @@ public class CentralControl extends JFrame {
 				System.exit(0);
 			}
 		});
-		
+
 		final JScrollPane imageScrollPane = new JScrollPane(imageView);
-//		if (image.width() <= 800 && image.height() <= 600)
-//			imageScrollPane.setPreferredSize(new Dimension(image.width(), image.height())); // set window size
-//		else
-//			imageScrollPane.setPreferredSize(new Dimension(600, 620));
-		
+		// if (image.width() <= 800 && image.height() <= 600)
+		// imageScrollPane.setPreferredSize(new Dimension(image.width(),
+		// image.height())); // set window size
+		// else
+		// imageScrollPane.setPreferredSize(new Dimension(600, 620));
+
 		jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		Image loadedImage = Mat2BufferedImage.mat2BI(image);
 		imageView.setIcon(new ImageIcon(loadedImage));
-		
+
 		JPanel jp1 = new JPanel();
 		jp1.setOpaque(false);
-		
+
 		JTextField processName = new JTextField("Process Name");
 		processName_manual = new JTextField(3);
 		JTextField machineNO = new JTextField("Machine NO");
-		machineNO_manual = new JTextField(3);
+		machineNO_manual = new JTextField(Integer.toString(mcNO), 3);
 		JTextField productName = new JTextField("Product Name");
-		productName_manual = new JTextField(3);
-
+		productName_manual = new JTextField(productN, 3);
 
 		processName.setEnabled(false); // true可以编辑
 		machineNO.setEnabled(false); // true可以编辑
 		productName.setEnabled(false); // true可以编辑
 
-//		jtf4.setFont(new Font("宋体", Font.BOLD | Font.ITALIC, 16)); // 字体，是否加粗、斜体，字号
-//		// 设置文本的水平对齐方式
-//		jtf4.setHorizontalAlignment(JTextField.CENTER);
+		// jtf4.setFont(new Font("宋体", Font.BOLD | Font.ITALIC, 16)); // 字体，是否加粗、斜体，字号
+		// // 设置文本的水平对齐方式
+		// jtf4.setHorizontalAlignment(JTextField.CENTER);
 
 		jp1.add(processName);
 		jp1.add(processName_manual);
@@ -135,30 +137,29 @@ public class CentralControl extends JFrame {
 		jp1.add(productName);
 		jp1.add(productName_manual);
 
-		
 		JPanel jp2 = new JPanel();
 		jp2.setOpaque(false);
 		JTextField binThresh = new JTextField("Binarization Threshold");
-		binarizationThreshold = new JTextField(3);
+		binarizationThreshold = new JTextField(Integer.toString(CentralControl.binThresh), 3);
 		JTextField ssThresh = new JTextField("SpotSpotter Threshold");
-		spotSpotterThreshold = new JTextField(3);
+		spotSpotterThreshold = new JTextField(Integer.toString(CentralControl.ssThresh), 3);
 		JTextField percent = new JTextField("%");
-		
+
 		binThresh.setEnabled(false); // true可以编辑
 		ssThresh.setEnabled(false); // true可以编辑
 		percent.setEnabled(false); // true可以编辑
-		
+
 		jp2.add(binThresh);
 		jp2.add(binarizationThreshold);
 		jp2.add(ssThresh);
 		jp2.add(spotSpotterThreshold);
 		jp2.add(percent);
 
-		jFrame.add(jp2, BorderLayout.SOUTH,0);
-		jFrame.add(jp1, BorderLayout.NORTH,0);
-		jFrame.add(imageScrollPane, BorderLayout.CENTER,-1);
+		jFrame.add(jp2, BorderLayout.SOUTH, 0);
+		jFrame.add(jp1, BorderLayout.NORTH, 0);
+		jFrame.add(imageScrollPane, BorderLayout.CENTER, -1);
 		jFrame.pack();
-//		jFrame.setLocationRelativeTo(null);
+		// jFrame.setLocationRelativeTo(null);
 		jFrame.setLocation(50, 120);
 		jFrame.setVisible(true);
 	}
