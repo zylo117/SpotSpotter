5e644322e1be12e4857eb3bc61337d47795cb06f
zylo117 <zylo117@hotmail.com>
2017-10-20 16:50:44 +0800

    Run.bat add

--- Run.bat
+++ Run.bat
@@ -1,2 +1,1 @@
-cd .\src 
-java -Djava.library.path=D:\workspace\SpotSpotter\src -Djava.ext.dirs=D:\workspace\SpotSpotter\lib pers.zylo117.spotspotter.mainprogram.Main
+java -jar -Xms1024m -Xmx1536m SpotSpotter.jar
